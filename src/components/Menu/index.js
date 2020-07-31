0101dd9ad754610ff090834ee68b29af8342775b
WagnerTerry <wagner.goncalves@calindra.com.br>
2020-07-31 00:28:47 -0300

    Avançando no projeto

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Logo from "../../assets/img/logo_aluraflix.png";
 // import ButtonLink from "../components/ButtonLink";
 import Button from "../Button/index";
@@ -7,11 +8,11 @@ import "./Menu.css";
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="AluraFlix Logo" />
-      </a>
+      </Link>
       {/* as = vai se comportar como a tag <a> */}
-      <Button as="a" className="ButtonLink" href="/">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo vídeo
       </Button>
     </nav>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Home from "./pages/Home/index";
+import CadastroVideo from "./pages/cadastro/video/index";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import CadastroCategoria from "./pages/cadastro/Categoria";
+
+const pagina404 = () => <div>Erro 404</div>;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // BrowserRouter rota do navegador
+  <BrowserRouter>
+    <Switch>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+
+      <Route path="/" component={Home} exact />
+      <Route component={pagina404} />
+    </Switch>
+  </BrowserRouter>,
+
   document.getElementById("root")
 );
